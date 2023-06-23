a1d7febca69bece115e65b8a359969ffad151e66
Karl-Gavois <Gavois.k@hotmail.com>
2023-06-23 13:02:02 +0200

    BG

---
+++ php/includes/select.php
@@ -0,0 +1,14 @@
+<div id="team1">
+    <h2>Équipe 1</h2>
+
+</div>
+<div>
+    <h2>Nom de faction</h2>
+</div>
+<div>
+    <h2>Équipe 2</h2>
+
+</div>
+<div id="selecteam1"></div>
+<div id="selecteam2"></div>
+<div id="selecteam3"></div>
