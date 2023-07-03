c3386afd70e89447e878812cc53ba6c18701e3ab
BKIslem <benkhaled.islem3@gmail.com>
2023-07-03 13:55:47 +0200

    fixer les conflits

--- php/script/script.js
+++ php/script/script.js
@@ -67,34 +67,4 @@ document.addEventListener('DOMContentLoaded', function() {
   resetDivs();
 });
 
-    document.addEventListener("DOMContentLoaded", function() {
-        var slider = document.querySelector(".slider");
-        var slides = slider.getElementsByClassName("slide");
-
-        var settings = {
-            slidesToShow: 3, // Nombre de slides affichées simultanément
-            slidesToScroll: 1, // Nombre de slides à faire défiler à la fois
-            // Ajoutez d'autres options selon vos besoins
-        };
-
-        // Fonction pour initialiser le slider
-        function initSlider() {
-            // Vérifie si le nombre de slides est supérieur au nombre de slides à afficher simultanément
-            if (slides.length > settings.slidesToShow) {
-                // Ajoutez des styles CSS appropriés pour créer le slider
-                slider.style.overflow = "hidden";
-                slider.style.whiteSpace = "nowrap";
-
-                // Parcours des slides et ajout de styles CSS
-                for (var i = 0; i < slides.length; i++) {
-                    slides[i].style.display = "inline-block";
-                    slides[i].style.verticalAlign = "top";
-                    slides[i].style.width = (100 / settings.slidesToShow) + "%";
-                }
-            }
-        }
-
-        // Appel de la fonction d'initialisation du slider
-        initSlider();
-    });
 
